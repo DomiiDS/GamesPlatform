7a13c2c47a6da9cf8b720434d63453636965316c
Mateusz Piórek <matpio8@st.amu.edu.pl>
2025-11-09 18:11:58 +0100

    added email validation and hid password input field

--- GamePlatformApp/models.py
+++ GamePlatformApp/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
+
+def ValidateUser(user):
+    pass
+
+def ValidatePassword(password):
+    pass
+
+def ValidateEmail(email):
+    if re.fullmatch(r"^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$", email) == None:
+        raise ValidationError(("Must be a valid email address"), code = "invalid")
 
 # Create your models here.
 
 class User(models.Model):
-    username = models.CharField(max_length = 100)
-    password = models.CharField(max_length = 100)
-    email = models.CharField(max_length = 100)
+    username = models.CharField(max_length = 100, validators=[ValidateUser])
+    password = models.CharField(max_length = 100, validators=[ValidatePassword])
+    email = models.CharField(max_length = 100, validators=[ValidateEmail])
 
     def __str__(self):
         return self.username
